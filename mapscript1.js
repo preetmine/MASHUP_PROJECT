4e6c3315cca9ea82c4aca284b84e137856268388
Amanpreet Kaur <744678@student.tor.loc>
2019-04-11 11:43:26 -0400

    commit

---
+++ mapscript1.js
@@ -0,0 +1,12 @@
+var map;
+function initialize(){
+var mapOptions={
+zoom:12,
+center:{lat:40.758904,lng:-73.985128}
+
+};
+
+        map=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions);
+      
+      
+}  google.maps.event.addDomListener(window,'load',initialize);
