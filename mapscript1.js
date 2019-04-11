45507eac5bda26186d10e98085dd0a107a174821
Amanpreet Kaur <744678@student.tor.loc>
2019-04-11 11:47:30 -0400

    commit

--- mapscript1.js
+++ mapscript1.js
@@ -6,7 +6,7 @@ center:{lat:40.758904,lng:-73.985128}
 
 };
 
-        map=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions);
+        map=new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
       
       
 }  google.maps.event.addDomListener(window,'load',initialize);
